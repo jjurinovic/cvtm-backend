8eea8e08641ab531a1479373c65080ff3c87077e
Juro Jurinovic <jjurinovic@gmail.com>
2024-01-10 23:09:55 +0100

    add roles to user endpoints

--- app/repository/user.py
+++ app/repository/user.py
@@ -1,9 +1,20 @@
 from sqlalchemy.orm import Session
-from .. import models, schemas, hashing
+from .. import models, schemas, hashing, roles
 from fastapi import HTTPException, status
 
 
-def create_user(req: schemas.User, db: Session):
+def same_company_id_and_no_root(company_id, user_company_id, role) -> bool:
+    if role != roles.Role.ROOT and user_company_id != company_id:
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN,
+                            detail=f"Company id must be same like your company id")
+
+
+def create_user(req: schemas.ShowUser, db: Session, current_user: schemas.User):
+
+    # don't allow admin to create user to another company
+    same_company_id_and_no_root(
+        req.company_id, current_user.company_id, current_user.role)
+
     hashed_pwd = hashing.Hash.bcrypt(req.password)
     new_user = models.User(name=req.name, email=req.email,
                            password=hashed_pwd, role=req.role, company_id=req.company_id)
@@ -13,15 +24,22 @@ def create_user(req: schemas.User, db: Session):
     return new_user
 
 
-def get_user(id: int, db: Session):
+def get_user(id: int, db: Session, current_user: schemas.User):
     user = db.query(models.User).filter(models.User.id == id).first()
 
-    if not user:
+    if current_user.role == roles.Role.ROOT:
+        return user
+
+    if user.company_id != current_user.company_id or not user:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                             detail=f"User with id {id} not found")
 
     return user
 
 
-def get_all_users(db: Session):
-    return db.query(models.User).all()
+def get_all_users(company_id: int, db: Session, current_user):
+    # don't allow admin to create user to another company
+    same_company_id_and_no_root(
+        company_id, current_user.company_id, current_user.role)
+
+    return db.query(models.User).filter(models.User.company_id == company_id)
--- app/routers/user.py
+++ app/routers/user.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter, Depends
-from .. import schemas, database
+from .. import schemas, database, roles, auth
 from sqlalchemy.orm import Session
 from ..repository import user
 from typing import List
@@ -10,16 +10,16 @@ router = APIRouter(
 )
 
 
-@router.post('/', response_model=schemas.ShowUser)
-def create_user(req: schemas.User, db: Session = Depends(database.get_db)):
-    return user.create_user(req, db)
+@router.post('/', response_model=schemas.ShowUser, dependencies=[Depends(auth.RoleChecker([roles.Role.ADMIN]))])
+def create_user(req: schemas.User, db: Session = Depends(database.get_db), current_user: schemas.User = Depends(auth.get_current_user)):
+    return user.create_user(req, db, current_user)
 
 
 @router.get('/{id}', response_model=schemas.ShowUser)
-def get_user(id: int, db: Session = Depends(database.get_db)):
-    return user.get_user(id, db)
+def get_user(id: int, db: Session = Depends(database.get_db), current_user: schemas.User = Depends(auth.get_current_user)):
+    return user.get_user(id, db, current_user)
 
 
-@router.get('/', response_model=List[schemas.ShowUser])
-def get_all_users(db: Session = Depends(database.get_db)):
-    return user.get_all_users(db)
+@router.get('/', response_model=List[schemas.ShowUser], dependencies=[Depends(auth.RoleChecker([roles.Role.ADMIN, roles.Role.MODERATOR]))])
+def get_all_users(company_id: int, db: Session = Depends(database.get_db), current_user: schemas.User = Depends(auth.get_current_user)):
+    return user.get_all_users(company_id, db, current_user)
