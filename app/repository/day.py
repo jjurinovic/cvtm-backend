1bb3738df43a247a4213d0f97e12d4fbc6b56a75
Juro Jurinovic <jjurinovic@gmail.com>
2024-01-13 15:30:13 +0100

    implemented some day and entry endpoints

--- app/repository/day.py
+++ app/repository/day.py
@@ -1,11 +1,17 @@
 from sqlalchemy.orm import Session
 from .. import models, schemas, roles
 from fastapi import HTTPException, status
+from datetime import datetime
 
 
-def create_day(req: schemas.Day, db: Session):
+def str_to_date(val: str):
+    return datetime.strptime(val, '%Y-%m-%d').date()
+
+
+def create_day(req: schemas.Day, db: Session, current_user: schemas.User):
+    date = str_to_date(req.date)
     new_day = models.Day(
-        date=req.date, company_id=req.company_id, user_id=req.user_id)
+        date=date, company_id=req.company_id, user_id=req.user_id)
     db.add(new_day)
     db.commit()
     db.refresh(new_day)
@@ -13,20 +19,47 @@ def create_day(req: schemas.Day, db: Session):
 
 
 def get_day(id: int, db: Session, current_user: schemas.User):
+    # find compy by id
+    day = db.query(models.Day).filter(models.Day.id == id).first()
+
+    if not day:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"Day with id {id} not found")
+
     # don't allow to return company if company not belongs to admin or user is not root
-    if id != current_user.company_id and current_user.role != roles.Role.ROOT:
+    if day.company_id != current_user.company_id and current_user.role != roles.Role.ROOT:
         raise HTTPException(status_code=status.HTTP_403_FORBIDDEN,
                             detail=f"Forbidden")
 
-    # find compy by id
-    company = db.query(models.Company).filter(models.Company.id == id).first()
-
-    if not company:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
-                            detail=f"Company with id {id} not found")
-
-    return company
+    return day
 
 
-def get_all_companies(db: Session):
-    return db.query(models.Company).all()
+def create_entry(req: schemas.TimeEntry, db: Session, current_user: schemas.User):
+    new_entry = models.TimeEntry(
+        start_time=req.start_time, end_time=req.end_time, pause=req.pause, notes=req.notes, day_id=req.day_id)
+    db.add(new_entry)
+    db.commit()
+    db.refresh(new_entry)
+    return new_entry
+
+
+def get_days(company_id: int, user_id: int, start, end, db):
+    # get all days for given company
+    days = db.query(models.Day).filter(models.Day.company_id == company_id)
+    # get all days for given user
+    days = days.filter(models.Day.user_id == user_id)
+
+    # get all days after given start
+    if start:
+        s = datetime.strptime(start, '%Y-%m-%d').date()
+        days = days.filter(models.Day.date >= s)
+
+    # get all days before given end
+    if end:
+        e = datetime.strptime(end, '%Y-%m-%d').date()
+        days = days.filter(models.Day.date <= e)
+
+    if not days:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail=f'not found {id}')
+    return days
--- app/routers/day.py
+++ app/routers/day.py
@@ -1,8 +1,9 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, status
 from .. import schemas, database, auth, roles
 from sqlalchemy.orm import Session
 from ..repository import day
 from typing import List
+from datetime import date
 
 router = APIRouter(
     tags=['Days'],
@@ -10,6 +11,22 @@ router = APIRouter(
 )
 
 
-@router.post('/', response_model=schemas.Day, dependencies=[Depends(auth.RoleChecker([roles.Role.ADMIN, roles.Role.MODERATOR, roles.Role.USER]))])
-def create_day(req: schemas.Day, db: Session = Depends(database.get_db)):
-    return day.create_day(req, db)
+@router.post('/', response_model=schemas.ShowDay)
+def create_day(req: schemas.Day, db: Session = Depends(database.get_db), current_user: schemas.User = Depends(auth.get_current_user)):
+    new_day = day.create_day(req, db, current_user)
+    return new_day
+
+
+@router.get('/{id}', response_model=schemas.ShowDay)
+def get_day(id: int, db: Session = Depends(database.get_db), current_user: schemas.User = Depends(auth.get_current_user)):
+    return day.get_day(id, db, current_user)
+
+
+@router.post('/entry', response_model=schemas.TimeEntry)
+def create_entry(req: schemas.TimeEntry, db: Session = Depends(database.get_db), current_user: schemas.User = Depends(auth.get_current_user)):
+    return day.create_entry(req, db, current_user)
+
+
+@router.get('/', status_code=status.HTTP_200_OK, response_model=List[schemas.ShowDay])
+def get_days(company_id: int, user_id: int, start: str, end: str, db: Session = Depends(database.get_db), current_user: schemas.User = Depends(auth.get_current_user)):
+    return day.get_days(company_id, user_id, start, end, db)
