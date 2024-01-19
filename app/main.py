19d68a51f7b59e6117eb2bab58f1b825ed24a832
Juro Jurinovic <jjurinovic@gmail.com>
2024-01-19 03:30:55 +0100

    added CORS

--- app/main.py
+++ app/main.py
@@ -3,8 +3,22 @@ from .routers import user, company, auth, day
 from . import models
 from .database import engine
 from datetime import date
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+origins = [
+    "http://localhost",
+    "http://localhost:4200",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 models.Base.metadata.create_all(engine)
 
