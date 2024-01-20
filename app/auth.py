c5126e73a39488e0acca0e588c1a557ee82f2b56
Juro Jurinovic <jjurinovic@gmail.com>
2024-01-20 23:17:32 +0100

    moved RoleChecker to roles file

--- app/auth.py
+++ app/auth.py
@@ -1,7 +1,6 @@
 from fastapi import Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer
-from . import token, models, database, roles
-from typing import List
+from . import token, models, database
 from sqlalchemy.orm import Session
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
@@ -20,19 +19,4 @@ def get_current_user(data: str = Depends(oauth2_scheme), db: Session = Depends(d
             token_data.username == models.User.email).first()
         if user is None:
             raise credentials_exception
-        return user
-
-
-class RoleChecker:
-    def __init__(self, permissions_required: List[roles.Role]):
-        self.permissions_required = permissions_required
-
-    def __call__(self, user: models.User = Depends(get_current_user)):
-        if user.role == roles.Role.ROOT:
-            return user
-
-        if user.role not in self.permissions_required:
-            raise HTTPException(
-                status_code=status.HTTP_403_FORBIDDEN,
-                detail="Not enough permissions to access this resource")
         return user
--- app/roles.py
+++ app/roles.py
@@ -1,8 +1,25 @@
+from fastapi import Depends, HTTPException, status
 from enum import Enum
+from .models import User
+from .auth import get_current_user
 
 
-class Role(str, Enum):
-    ADMIN = "ADMIN"
-    MODERATOR = "MODERATOR"
-    USER = "USER"
-    ROOT = "ROOT"
+class Role(int, Enum):
+    ROOT = 0
+    ADMIN = 1
+    MODERATOR = 2
+    USER = 3
+
+
+class RoleChecker:
+    def __init__(self, role_required: Role):
+        self.role_required = role_required
+
+    def __call__(self, user: User = Depends(get_current_user)):
+        print(type(user.role))
+        print(type(self.role_required.value))
+        if user.role > self.role_required.value:
+            raise HTTPException(
+                status_code=status.HTTP_403_FORBIDDEN,
+                detail="Not enough permissions to access this resource")
+        return user
