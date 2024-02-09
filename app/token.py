6337406d8d375ccbf10f59671779da847978243b
Juro Jurinovic <jjurinovic@gmail.com>
2024-02-09 18:52:19 +0100

    changed token expire time

--- app/token.py
+++ app/token.py
@@ -8,7 +8,7 @@ from .schemas.auth import TokenData
 # openssl rand -hex 32
 SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
 ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 15
+ACCESS_TOKEN_EXPIRE_MINUTES = 60
 
 
 def create_access_token(data: dict, expires_delta: Optional[timedelta] = None):
