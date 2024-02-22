ee726bba8423f1d285ac7550472c4bda6355e1b6
Juro Jurinovic <jjurinovic@gmail.com>
2024-02-22 17:16:01 +0100

    changed main file

--- app/main.py
+++ app/main.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+import uvicorn
+import os
 
 from .auth.router import AuthRouter
 from .users.router import UsersRouter
@@ -29,3 +31,8 @@ app.include_router(AuthRouter)
 app.include_router(UsersRouter)
 app.include_router(CompanyRouter)
 app.include_router(TimeEntryRouter)
+
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0", port=int(
+        os.environ.get('PORT', 8000)), log_level="info")
